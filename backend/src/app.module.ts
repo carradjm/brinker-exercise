25e5e7e3ff1142281ea4f03e737f79be571f3b4d
Malcolm Carradine <malcolm.carradine@gmail.com>
2024-10-14 18:18:52 -0500

    Adds products as a resource; update app.module.ts to use SQLite

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Automatically syncs entities with the database
+    }),
+    ProductsModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
