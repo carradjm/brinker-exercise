d99faa8458ccbbf9e71338f65a5fc3c8fdc17ef4
Malcolm Carradine <malcolm.carradine@gmail.com>
2024-10-15 12:57:10 -0500

    Updates backend main.ts to accept front-end API calls

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Frontend URL
+    credentials: true,
+  });
+  await app.listen(3001);
 }
 bootstrap();
